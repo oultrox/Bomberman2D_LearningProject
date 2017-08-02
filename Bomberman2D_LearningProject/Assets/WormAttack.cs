ce3c01e71aafe03eb338f05c52bdefa790081685
oultrox <jesus-perez95@hotmail.com>
2017-08-02 15:22:05 -0400

    [add] Enemies can kill the player now

---
+++ Bomberman2D_LearningProject/Assets/WormAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormAttack : MonoBehaviour {
+    [SerializeField] private int damage;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Health hp = collision.gameObject.GetComponent<Health>();
+            if (hp)
+            {
+                hp.TakenDamage(damage);
+                Debug.Log("Supuestamente dañando.");
+            }
+        }
+    }
+}
